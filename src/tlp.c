1affd429055f81fdcf0f7f6c3cabce6b7948a19e
Freshdouble <peterkremsner@gmx.at>
2016-05-03 00:51:42 +0200

    Removed Libfifo

--- src/tlp.c
+++ src/tlp.c
@@ -6,3 +6,60 @@
  */
 
 
+#include "tlp.h"
+
+void tlp_init(tlp_t *tlp, tlp_frameRecieved recieveCallbackFunction)
+{
+	tlp->data_to_acknowledge = 0;
+	tlp->last_transmitted_sequence = 0;
+	tlp->callback = recieveCallbackFunction;
+}
+
+void tlp_ack(uint8_t number)
+{
+	if(number != 0)
+	{
+
+	}
+}
+
+uint8_t tlp_recieve(tlp_t *tlp, uint8_t *data, uint8_t size)
+{
+	uint8_t i;
+	tlp_message_t message;
+
+	if(size < 2)
+		return 0;
+
+	tlp->data_to_acknowledge = data[0];
+	tlp_ack(data[1]);
+
+	for(i = 2; i < size; i++)
+	{
+		message.data[i] = data[i];
+	}
+	message.size = size - 2;
+
+	return tlp->callback(&message);
+}
+
+uint8_t tlp_send(tlp_t *tlp, uint8_t *data, uint8_t size)
+{
+	tlp_message_t message;
+	int i;
+
+	if(size > TLP_MESSAGE_SIZE)
+		return 0;
+
+	message.data[0] = tlp->last_transmitted_sequence = tlp->last_transmitted_sequence + 1;
+	message.data[1] = tlp->data_to_acknowledge;
+
+	for(i = 0; i < size; i++)
+	{
+		message.data[i + 2] = data[i];
+	}
+
+	message.size = size + 2;
+
+	return 0;
+}
--- src/tlp.h
+++ src/tlp.h
@@ -8,6 +8,32 @@
 #ifndef SRC_TLP_H_
 #define SRC_TLP_H_
 
+#include "../libfifo/c/stc_fifo.h"
+#include <inttypes.h>
 
+#define TLP_MESSAGE_SIZE 31
+#define WINDOW_SIZE 8
+
+typedef struct
+{
+	uint8_t data[TLP_MESSAGE_SIZE + 2];
+	uint8_t size;
+}tlp_message_t;
+
+typedef char (*tlp_frameRecieved)(tlp_message_t *message);
+typedef char (*tlp_frameSend)(uint8_t *buffer, uint8_t size);
+
+typedef struct
+{
+	struct
+	{
+		tlp_message_t buffer[WINDOW_SIZE];
+
+	}transmit_buffer;
+	tlp_frameRecieved callback;
+	tlp_frameSend framesendFunction;
+	uint8_t data_to_acknowledge;
+	uint8_t last_transmitted_sequence;
+}tlp_t;
 
 #endif /* SRC_TLP_H_ */
