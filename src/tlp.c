7bcd0fdd8db0b27bbacd0c8cfd5598811b040d29
Peter Kremsner <peterkremsner@gmx.at>
2016-05-06 00:16:18 +0200

    Update

--- src/tlp.c
+++ src/tlp.c
@@ -8,18 +8,35 @@
 
 #include "tlp.h"
 
-void tlp_init(tlp_t *tlp, tlp_frameRecieved recieveCallbackFunction)
+void tlp_init(tlp_t *tlp, tlp_frameRecieved recieveCallbackFunction, tlp_frameSend framesendFunction)
 {
 	tlp->data_to_acknowledge = 0;
 	tlp->last_transmitted_sequence = 0;
+	tlp->last_recieved_sequence = 0;
+	tlp->ack_counter = ACK_COUNTER;
 	tlp->callback = recieveCallbackFunction;
+	tlp->framesendFunction = framesendFunction;
+	fifo_init(&tlp->transmit_buffer.fifo,tlp->transmit_buffer.buffer,
+			WINDOW_SIZE,sizeof(tlp_message_t));
 }
 
-void tlp_ack(uint8_t number)
+void tlp_recv_ack(uint8_t number, tlp_t *tlp)
 {
-	if(number != 0)
+	int i;
+	tlp_message_t* message;
+	if(number != 0) //If ACK is 0 it isn't valid ACK Field
 	{
-
+		for(i = 0; i < WINDOW_SIZE; i++)
+		{
+			message = fifo_get_nth_Object(i,&tlp->transmit_buffer.fifo);
+			if(message->data[0] == number)
+			{
+				fifo_delete_n_Objects(i,&tlp->transmit_buffer.fifo);
+				if(fifo_empty(&tlp->transmit_buffer.fifo))
+					tlp->data_to_acknowledge = 0;
+				break;
+			}
+		}
 	}
 }
 
@@ -31,31 +48,49 @@ uint8_t tlp_recieve(tlp_t *tlp, uint8_t *data, uint8_t size)
 	if(size < 2)
 		return 0;
 
-	if((tlp->last_recieved_sequence + 1) != data[0]) //Check if frame is in the right order
+	if((tlp->last_recieved_sequence + 1) > data[0] || (tlp->last_recieved_sequence - WINDOW_SIZE) > data[0]) //Check if frame is in the right order
 		return 0;
 
-	tlp->last_recieved_sequence = data[0]; //Update recieve Order
-	tlp->data_to_acknowledge = data[0];    //Ack byte
-	tlp_ack(data[1]);
-
-	for(i = 2; i < size; i++)
+	tlp_recv_ack(data[1], tlp);
+	if(size != 2) // If size == 2 recieved a simple ack Message
 	{
-		message.data[i] = data[i];
-	}
-	message.size = size - 2;
+		tlp->last_recieved_sequence = data[0]; //Update recieve Order
 
-	return tlp->callback(&message);
+		if(data[1] != 0) //If ACK is 0 it isn't valid ACK Field
+		{
+			if(data[1] < tlp->last_recieved_sequence)
+				//Acknowledge with last recieved sequence number
+				tlp->data_to_acknowledge = tlp->last_recieved_sequence;
+			else
+				//Acknowledge with current sequence number
+				tlp->data_to_acknowledge = data[0];    //Ack byte
+		}
+
+		for(i = 2; i < size; i++)
+		{
+			message.data[i] = data[i];
+		}
+		message.size = size - 2;
+
+		return tlp->callback(&message);
+	}
+	return 1;
 }
 
 uint8_t tlp_send(tlp_t *tlp, uint8_t *data, uint8_t size)
 {
 	tlp_message_t message;
+	uint8_t increment_sequence;
 	int i;
 
 	if(size > TLP_MESSAGE_SIZE)
 		return 0;
 
-	message.data[0] = tlp->last_transmitted_sequence = tlp->last_transmitted_sequence + 1;
+	increment_sequence = tlp->last_transmitted_sequence + 1;
+	if(increment_sequence == 0)
+		increment_sequence = 1;
+
+	message.data[0] = increment_sequence;
 	message.data[1] = tlp->data_to_acknowledge;
 
 	for(i = 0; i < size; i++)
@@ -64,6 +99,84 @@ uint8_t tlp_send(tlp_t *tlp, uint8_t *data, uint8_t size)
 	}
 
 	message.size = size + 2;
+	message.resendCounter = RESEND_COUNTER;
+	message.timeoutCounter = TIMEOUT;
+
+	if(fifo_write_object(&message,&tlp->transmit_buffer.fifo))
+	{
+		if(tlp->framesendFunction(message.data,message.size))
+		{
+			tlp->ack_counter = ACK_COUNTER;
+			tlp->last_transmitted_sequence = increment_sequence;
+			return size;
+		}
+	}
 
 	return 0;
 }
+
+void tlp_send_ack(tlp_t *tlp)
+{
+	uint8_t message[2];
+	if(tlp->data_to_acknowledge)
+	{
+		message[0] = tlp->last_transmitted_sequence; //Pick a valid sequence number
+		message[1] = tlp->data_to_acknowledge;
+		if(tlp->framesendFunction(message,2)) // Dont save this message, just transmit it
+		{
+			tlp->ack_counter = ACK_COUNTER;
+		}
+	}
+	else
+	{
+		tlp->ack_counter = ACK_COUNTER;
+	}
+}
+
+void tlp_tick(tlp_t *tlp)
+{
+	uint16_t fifo_data;
+	tlp_message_t* message;
+
+	if(tlp->ack_counter == 0)
+	{
+		tlp_send_ack(tlp);
+	}
+	else
+	{
+		tlp->ack_counter--;
+	}
+
+	fifo_data = fifo_datasize(&tlp->transmit_buffer.fifo);
+
+	while(fifo_data > 0)
+	{
+		message = fifo_get_nth_Object(fifo_data - 1,&tlp->transmit_buffer.fifo);
+		if(message->resendCounter == 0)
+		{
+			if(tlp->framesendFunction(message->data,message->size))
+			{
+				message->resendCounter = RESEND_COUNTER;
+			}
+			else
+			{
+				if(message->timeoutCounter == 0)
+				{
+					if(tlp->timeoutCallback(message))
+						tlp->timeoutCallback(message);
+					message->timeoutCounter = TIMEOUT;
+				}
+				else
+				{
+					message->timeoutCounter--;
+				}
+			}
+		}
+		else
+		{
+			message->resendCounter--;
+		}
+		fifo_data--;
+	}
+}
+
--- src/tlp.h
+++ src/tlp.h
@@ -9,33 +9,40 @@
 #define SRC_TLP_H_
 
 #include <inttypes.h>
+#include "../libfifo/c/stc_fifo.h"
 
 #define TLP_MESSAGE_SIZE 31
 #define WINDOW_SIZE 8
 #define RESEND_COUNTER 8
+#define ACK_COUNTER RESEND_COUNTER - 2 //This must be smallern than the RESEND_COUNTER
+#define TIMEOUT 10 //Number of retries to deliver a message until the timeoutCallback gets executed
 
 typedef struct
 {
 	uint8_t data[TLP_MESSAGE_SIZE + 2];
 	uint8_t size;
-	uint8_t sequenceNumber;
 	uint8_t resendCounter;
+	uint8_t timeoutCounter;
 }tlp_message_t;
 
 typedef char (*tlp_frameRecieved)(tlp_message_t *message);
 typedef char (*tlp_frameSend)(uint8_t *buffer, uint8_t size);
+typedef char (*tlp_timeOutCallback)(tlp_message_t* message);
 
 typedef struct
 {
 	struct
 	{
 		tlp_message_t buffer[WINDOW_SIZE];
+		stc_fifo_t fifo;
 	}transmit_buffer;
 	tlp_frameRecieved callback;
 	tlp_frameSend framesendFunction;
+	tlp_timeOutCallback timeoutCallback;
 	uint8_t data_to_acknowledge;
 	uint8_t last_recieved_sequence;
 	uint8_t last_transmitted_sequence;
+	uint8_t ack_counter;
 }tlp_t;
 
 #endif /* SRC_TLP_H_ */
